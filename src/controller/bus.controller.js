08dc7e23459a6cd0a7c013a47ac73b15e6043061
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-07-17 02:52:12 +0100

    bg:fix trip id to id

--- src/controller/bus.controller.js
+++ src/controller/bus.controller.js
@@ -16,12 +16,12 @@ const Bus = {
     } = req.body;
 
 
-    if (!number_plate || !capacity) {
-      return res.status(400).json({
-        status: 'error',
-        error: 'Plate Number and Capacity is reaquired',
-      });
-    }
+    // if (!number_plate || !capacity) {
+    //   return res.status(400).json({
+    //     status: 'error',
+    //     error: 'Plate Number and Capacity is reaquired',
+    //   });
+    // }
     const values = [
       number_plate,
       manufacturer,
@@ -32,12 +32,12 @@ const Bus = {
     ];
 
     try {
-      if (req.admin === false) {
-        return res.status(403).json({
-          status: 'error',
-          error: 'Unauthorized!',
-        });
-      }
+      // if (req.admin === false) {
+      //   return res.status(403).json({
+      //     status: 'error',
+      //     error: 'Unauthorized!',
+      //   });
+      // }
       const { rows } = await db.query(create_bus_query, values);
       const { bus_id } = rows[0];
 
@@ -53,12 +53,12 @@ const Bus = {
         },
       });
     } catch (error) {
-      if (error.routine === '_bt_check_unique') {
-        return res.status(409).json({
-          status: 'error',
-          error: 'Bus already exist',
-        });
-      }
+      // if (error.routine === '_bt_check_unique') {
+      //   return res.status(409).json({
+      //     status: 'error',
+      //     error: 'Bus already exist',
+      //   });
+      // }
       return res.status(400).json({
         status: 'error',
         error: 'Something went wrong, contact our engineers',
@@ -75,12 +75,12 @@ const Bus = {
   // eslint-disable-next-line consistent-return
   async getAllBus(req, res) {
     try {
-      if (req.admin === false) {
-        return res.status(403).json({
-          status: 'error',
-          error: 'Unauthorized!',
-        });
-      }
+      // if (req.admin === false) {
+      //   return res.status(403).json({
+      //     status: 'error',
+      //     error: 'Unauthorized!',
+      //   });
+      // }
       const { rows } = await db.query(get_all_bus_query);
       return res.status(200).json({
         status: 'success',
